f2a4392ef4cf1d8e4e8592b66616214b0661283c
Pouya Pezeshkpour <pezeshkp@uci.edu>
2026-04-20 13:41:15 -0700

    Add files via upload

--- src/autopyverifier/__init__.py
+++ src/autopyverifier/__init__.py
@@ -1,6 +1,22 @@
-"""AutoVerifier: DAG search for minimum feasible verifier sets."""
+"""Public package interface for AutoPyVerifier."""
+
+from . import config, data, execution, metrics, models, prompts, search
+from .config import LLMConfig, SearchConfig
+from .data import format_examples_for_prompt, load_devset
+from .models import (
+    Aggregator,
+    AtomicVerifier,
+    EvalStats,
+    Example,
+    SearchResult,
+    VerifierSetProgram,
+)
+from .search import AutoVerifierSearch
+
+__version__ = "0.1.0"
 
 __all__ = [
+    "__version__",
     "config",
     "data",
     "execution",
@@ -8,4 +24,15 @@ __all__ = [
     "models",
     "prompts",
     "search",
+    "Aggregator",
+    "AtomicVerifier",
+    "AutoVerifierSearch",
+    "EvalStats",
+    "Example",
+    "LLMConfig",
+    "SearchConfig",
+    "SearchResult",
+    "VerifierSetProgram",
+    "format_examples_for_prompt",
+    "load_devset",
 ]
